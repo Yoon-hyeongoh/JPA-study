1aa9547c884cf945c21c5b24ad75fe634bd4e32a
hyeongoh.yoon <sksguddh@gmail.com>
2022-11-21 15:49:59 +0900

    영속성 관리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,8 +5,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-import java.util.List;
-
 public class JpaMain {
     public static void main(String[] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -17,26 +15,15 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //Member member = new Member();
-            //member.setId(2L);
-            //member.setName("helloB");
-            //em.persist(member);
+            Member m = em.find(Member.class, 1L);
+            m.setName("AAAAAAAAAAA");
 
-            //Member findMember = em.find(Member.class, 1L);
-            //findMember.setName("HelloA");
-            //System.out.println("findMember.id " + findMember.getId());
-            //System.out.println("findMember.name " + findMember.getName());
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            em.clear();
 
-            for(Member m : result){
-                System.out.println("member.name :: " + m.getName());
-                System.out.println("member.name :: " + m.getId());
-            }
+            Member m2 = em.find(Member.class, 1L);
 
             tx.commit();
+            System.out.println("AFTER");
         } catch(Exception e){
             tx.rollback();
         } finally {
